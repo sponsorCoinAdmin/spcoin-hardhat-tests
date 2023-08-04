73cbe1a2199526453d3cc2e52c2908f3c5de3cc4
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-08-04 15:33:12 -0400

    Update spCoinSetup.sh

--- spCoinEnvSetup/spCoinSetup.sh
+++ spCoinEnvSetup/spCoinSetup.sh
@@ -1,8 +1,5 @@
 # Requires git setup
-export SPONSOR_COIN_ROOT_DIR=$PWD
-export SPONSOR_COIN_DIR=sponsorCoinHHTests;
-export SPONSOR_COIN_DEV_DIR=spCoinEnvSetup;
-echo "DownLoading Sponsor Coin Development Envirement from gitHub"
-git clone --recurse-submodules git@github.com:sponsorCoinAdmin/$SPONSOR_COIN_DIR.git
-cd $SPONSOR_COIN_DIR
+
+git clone --recurse-submodules git@github.com:sponsorCoinAdmin/spCoinHardHatTests.git
+cd ./spCoinHardHatTests/spCoinEnvSetup
 . ./spCoinEnvSetup.sh
