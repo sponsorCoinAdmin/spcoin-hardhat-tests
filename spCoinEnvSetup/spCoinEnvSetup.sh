46e72dcf54e9a939d5648da0205ec0ecb47651ee
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-08-04 15:56:38 -0400

    Update spCoinEnvSetup.sh

--- spCoinEnvSetup/spCoinEnvSetup.sh
+++ spCoinEnvSetup/spCoinEnvSetup.sh
@@ -3,15 +3,16 @@ export SPONSOR_COIN_DIR=${SPONSOR_COIN_ENV%/*};
 export SPONSOR_COIN_ROOT=${SPONSOR_COIN_DIR%/*};
 export SPONSOR_COIN_LOGS=$SPONSOR_COIN_ENV/logs
 
-echo "Adding startup configuration Files to Sponsor Coin environment setup file $SPONSOR_COIN_ENV/.e"
+echo "Installing the Node Libraries"
+cd $SPONSOR_COIN_DIR
+npm i
+
+echo "Adding startup environment configuration Files For Sponsor Coin $SPONSOR_COIN_ENV/.e"
+cp $SPONSOR_COIN_ENV/.e_ORIG $SPONSOR_COIN_ENV/.e
 echo "export SPONSOR_COIN_ENV=$SPONSOR_COIN_ENV"   | tee -a $SPONSOR_COIN_ENV/.e
 echo "export SPONSOR_COIN_HOME=$SPONSOR_COIN_HOME" | tee -a $SPONSOR_COIN_ENV/.e
 echo "export SPONSOR_COIN_LOG=$SPONSOR_COIN_LOGS"  | tee -a $SPONSOR_COIN_ENV/.e
 echo "export SPONSOR_COIN_ROOT=$SPONSOR_COIN_ROOT" | tee -a $SPONSOR_COIN_ROOT/.e
-
-echo "Installing the Node Libraries"
-cd $SPONSOR_COIN_DIR
-npm i
 
 echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.baschrc"
 echo ". "$SPONSOR_COIN_ENV"/.e" | tee -a ~/.bashrc
