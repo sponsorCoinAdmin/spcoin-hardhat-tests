9dfd68186e5f4b9ccbd78bb9e6a4a49d794647b7
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-08-15 22:57:01 -0400

    WIP

--- env/JUNK/.m
+++
@@ -1,9 +0,0 @@
-clear
-echo "=========================================================================="
-echo "                       ALIAS MENU SETTINGS"
-echo "=========================================================================="
-echo "m  = Display this Alias Settings"
-echo "em = Environment Menu Alias Settings"
-echo "am = Alias Menu Setup's"
-echo "tm = Testing Menu Alias"
-echo "=========================================================================="
