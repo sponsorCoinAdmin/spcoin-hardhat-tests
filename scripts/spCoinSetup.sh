c53996a405449eae4244f7ab2634b1acdf2a74ec
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-08-17 13:07:49 -0400

    WIP

--- scripts/spCoinSetup.sh
+++ scripts/spCoinSetup.sh
@@ -7,4 +7,4 @@ repo=spCoinHardHatTests
 git clone --recurse-submodules git@github.com:sponsorCoinAdmin/$repo.git
 cd ./$repo/setupEnv
 . ./spCoinEnvSetup.sh | tee -a ../logs/spCoinEnvSetup.log
-cd $currDir
+cd $repo
